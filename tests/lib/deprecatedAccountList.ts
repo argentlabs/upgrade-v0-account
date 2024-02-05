1338545ac971c61099c54529e2e983b71284da03
Sergio Garcia <sergio@argent.xyz>
2024-02-05 15:44:06 +0000

    support for older versions

--- tests/lib/deprecatedAccountList.ts
+++ tests/lib/deprecatedAccountList.ts
@@ -1,21 +1,15 @@
-export const latestAccountClassHash_V_0_2_3_1 = "0x033434ad846cdd5f23eb73ff09fe6fddd568284a0fb7d1be20ee482f044dabe2";
-export const v0_2_2_implementation = "0x3e327de1c40540b98d05cbcb13552008e36f0ec8d61d46956d2f9752c294328";
+export const latestAccountClassHash_V_0_2_3_1_classHash =
+  "0x33434ad846cdd5f23eb73ff09fe6fddd568284a0fb7d1be20ee482f044dabe2";
+export const latestAccountClassHash_V_0_2_3_1_address =
+  "0x014297b4d1d302438fc420f7983e78541af79de07aa1f0c4b168d80dbf909d5e";
 
-export const deprecatedAccountList = [
-  {
-    version: "0.2.2",
-    implementation: v0_2_2_implementation,
-    proxy: "0x25ec026985a3bf9d0cc1fe17326b245dfdc3ff89b8fde106542a3ea56c5a918",
-  },
-  // TODO upgrading from versions older than 0.2.2 is not tested yet
-  // {
-  //   version: "0.2.0",
-  //   implementation: "0x07595b4f7d50010ceb00230d8b5656e3c3dd201b6df35d805d3f2988c69a1432",
-  //   proxy: "0x071c3c99f5cf76fc19945d4b8b7d34c7c5528f22730d56192b50c6bbfd338a64",
-  // },
-  // {
-  //   version: "0.2.1",
-  //   implementation: "0x06a1776964b9f991c710bfe910b8b37578b32b26a7dffd1669a1a59ac94bf82f",
-  //   proxy: "0x08f1dae4382de84c1ab18cc73d38578f3ef70b2174ac9a54a1c4ae165ea668c",
-  // },
-];
+export const v0_2_2_implementationClassHash = "0x3e327de1c40540b98d05cbcb13552008e36f0ec8d61d46956d2f9752c294328";
+export const v0_2_2_proxyClassHash = "0x25ec026985a3bf9d0cc1fe17326b245dfdc3ff89b8fde106542a3ea56c5a918";
+
+export const v0_2_0_proxyClassHash = "0x71c3c99f5cf76fc19945d4b8b7d34c7c5528f22730d56192b50c6bbfd338a64";
+
+export const v0_2_1_implementationAddress = "0x1bd7ca87f139693e6681be2042194cf631c4e8d77027bf0ea9e6d55fc6018ac";
+export const v0_2_1_implementationClassHash = "0x6a1776964b9f991c710bfe910b8b37578b32b26a7dffd1669a1a59ac94bf82f";
+
+export const v0_2_0_implementationAddress = "0x5f28c66afd8a6799ddbe1933bce2c144625031aafa881fa38fa830790eff204";
+export const v0_2_0_implementationClassHash = "0x7595b4f7d50010ceb00230d8b5656e3c3dd201b6df35d805d3f2988c69a1432";
