47f096523bc8f72a0f57c413f30bf92eae3e5fb4
diegodelrieue <diego@argent.xyz>
2024-02-13 14:42:38 +0100

    chore: max required improvements

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,12 @@ export const metadata: Metadata = {
   description: "Upgrade deprecated Argent accounts",
 };
 
+export const Navbar = () => (
+  <nav className=" text-white">
+    <Image alt="Argent Logo" src="/argent-logo-colour.png" width={200} height={200} />
+  </nav>
+);
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -16,7 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} `}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
