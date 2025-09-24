2af81388de84e92db14230c91cadce1de26f3f99
Sergio Garcia <sergio@argent.xyz>
2025-09-24 11:07:59 +0100

    update text

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <div className="flex items-center justify-center p-5 ">
         <div className="flex flex-col m-24 max-w-5xl">
           <h5 className="text-2xl  text-gray-950 text-center font-barlow">
-            Upgrade your deprecated account to the latest one
+            Upgrade your deprecated account
           </h5>
           <h4 className="text-1xl text-gray-950 text-center py-2 mb-5">
             ⚠️ This is a rare exception that we provide a tool that asks for your private key. As an alternative you can
